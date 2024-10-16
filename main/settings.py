48bf3765f730dc4359b450de2158cb2eec3e0328
Viktoria0228 <vikacold2010@gmail.com>
2024-10-16 14:00:28 +0300

    Tour content added

--- main/settings.py
+++ main/settings.py
@@ -1,6 +1,12 @@
-import flask
+import flask, flask_sqlalchemy, flask_migrate
 
 project = flask.Flask(
     import_name = "main",
     template_folder = "templates"
 )
+
+project.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+
+DATABASE = flask_sqlalchemy.SQLAlchemy(app = project)
+
+migrate = flask_migrate.Migrate(app = project, db = DATABASE)
--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
 import tour_page
 from .settings import project
 
-tour_page.tour.add_url_rule(rule = "/tour", view_func = tour_page.render_tour)
+tour_page.tour.add_url_rule(rule = "/tour", view_func = tour_page.render_tour, methods = ["GET", "POST"])
+tour_page.tour.add_url_rule(rule = "/tour_info/<int:id>", view_func = tour_page.render_tour_info, methods = ["GET", "POST"])
+
 project.register_blueprint(blueprint = tour_page.tour)
+
 
