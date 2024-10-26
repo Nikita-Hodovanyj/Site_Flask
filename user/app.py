f6df566bce200f2479609c148510002b45fefda8
Nikita-Hodovanyj <nikitagodovanyj@gmail.com>
2024-10-26 12:30:27 +0300

    creat css for user

--- user/app.py
+++ user/app.py
@@ -3,6 +3,8 @@ import flask
 user_page = flask.Blueprint(
     name = "user_page",
     import_name ="user",
-    template_folder = "templates"
+    template_folder = "templates",
+    static_folder = "static",
+    static_url_path = "/user"
 
 )
